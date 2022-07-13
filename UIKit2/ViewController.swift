e5de4303ad7d73e0e75b321c2e7b64a4c4a45a9b
Evgeniy Kruglov <evgkukharenko@gmail.com>
2022-07-13 14:22:34 +0300

    add button 1

--- UIKit2/ViewController.swift
+++ UIKit2/ViewController.swift
@@ -11,33 +11,34 @@ class ViewController: UIViewController {
     
     let mySwitch2 = UISwitch()
     let button = UIButton()
+    let button1 = UIButton()
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.button1.frame = CGRect(x: 120, y: 250, width: 150, height: 30)
+        self.button1.backgroundColor = .orange
+        self.button1.setTitle("ok", for: .normal)
+        self.button1.setTitle("button press", for: .highlighted)
+        self.view.addSubview(button1)
+        
         //made button
         self.button.frame = CGRect.init(x: 120, y: 200, width: 150, height: 30)
- 
-            //color button
         self.button.backgroundColor = .orange
-                    //
         self.button.setTitle("ok", for: .normal)
         self.button.setTitle("button press", for: .highlighted)
-                        //added button
         self.view.addSubview(button)
 
-        //made switch
         self.mySwitch2.frame = CGRect.zero
         self.mySwitch2.center = self.view.center
-            //color switch
         self.mySwitch2.tintColor = .red
         self.mySwitch2.thumbTintColor = .yellow
         self.mySwitch2.onTintColor = .blue
-                //added switch
         self.view.addSubview(mySwitch2)
         self.mySwitch2.addTarget(self, action: #selector(isOn(target:)), for: .valueChanged)
+        
         
     }
     
