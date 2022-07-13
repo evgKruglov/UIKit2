bb101cfb3edfe8aa5969fb682e234f81927f6e3c
Evgeniy Kruglov <evgkukharenko@gmail.com>
2022-07-13 14:28:48 +0300

    add print on second view controller

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -11,7 +11,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("this is main")
         // Do any additional setup after loading the view.
     }
     
